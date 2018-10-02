bab37c9340788e650861b3ddc72935ea0a715724
Jordan Adams <hi@jordanadams.co.uk>
2018-10-02 21:17:21 +0100

    improve module interop support

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import {
+  getAvailableDocs,
+  getDocsForSlug,
+  getIndexForSlug,
+  getMetadataForSlug
+} from './client';
+
 export * from './client';
+
+export default {
+  getAvailableDocs,
+  getDocsForSlug,
+  getIndexForSlug,
+  getMetadataForSlug
+};
